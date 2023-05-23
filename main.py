ee0d1798cb9abb91f2271a77758faf99a84f310c
Malg Eum Yu <yumalg12@gmail.com>
2023-05-23 14:46:52 +0900

    Commit2 빼기 함수를 추가함

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ def add_func(n1, n2):
     result = n1 + n2
     return result
 
+def sub_func(n1, n2):
+    result = n1 - n2
+    return result
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
 ## 메인 코드부
 res = add_func(num1, num2)
 print(num1, "+", num2, "=", res)
+
+res = sub_func(num1, num2)
+print(num1, "-", num2, "=", res)
+
