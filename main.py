dc7db847cbfb0efd2769de580f82f8bcdac329c8
Malg Eum Yu <yumalg12@gmail.com>
2023-05-23 14:52:34 +0900

    Commit4 나누기 함수 추가

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def mul_func(n1, n2):
     result = n1 * n2
     return result
 
+def div_func(n1, n2):
+    result = n1 / n2
+    return result
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -23,3 +27,6 @@ print(num1, "-", num2, "=", res)
 
 res = mul_func(num1, num2)
 print(num1, "*", num2, "=", res)
+
+res = div_func(num1, num2)
+print(num1, "/", num2, "=", res)
